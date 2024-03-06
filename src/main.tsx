f5102157cce7e20368af23ffe10c7024c27b14f0
ADITYA SINGH SIKARWAR <bs151439@gmail.com>
2024-03-06 10:39:12 +0530

    main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -8,8 +8,8 @@ import { AuthProvider } from './context/AuthContext.jsx'
 import { Toaster } from 'react-hot-toast'
 import axios from 'axios'
 
-// axios.defaults.baseURL= "http://localhost:5000/api/v1";
-axios.defaults.baseURL= `${window.location.origin}/api/v1`;
+axios.defaults.baseURL= "http://localhost:5000/api/v1";
+// axios.defaults.baseURL= `${window.location.origin}/api/v1`;
 axios.defaults.withCredentials= true;
 
 const theme = createTheme({
